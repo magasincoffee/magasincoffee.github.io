fbac0e9b6aba2e20fd9587b54e6d0630782b2e20
MAGASIN <bachvantoi1994@gmail.com>
2026-09-20 09:42:17 +0700

    release(supervisor): align v51 runtime fingerprints

--- 08_INTEGRATIONS/supervisor/windows/repair-supervisor.ps1
+++ 08_INTEGRATIONS/supervisor/windows/repair-supervisor.ps1
@@ -20,7 +20,7 @@ $target = Join-Path $root 'target.json'
 $pidFile = Join-Path $root 'supervisor.pid'
 $logFile = Join-Path $root 'supervisor.log'
 $projectStateUrl = 'https://raw.githubusercontent.com/magasincoffee/magasincoffee.github.io/main/01_DOCS/MAGASIN/00_PROJECT_STATE.json'
-$expectedRuntimeVersion = '2026-09-19.50'
+$expectedRuntimeVersion = '2026-09-19.51'
 
 if (-not (Test-Path $sourceLifecycle)) { throw "Lifecycle truth helper missing: $sourceLifecycle" }
 . $sourceLifecycle
